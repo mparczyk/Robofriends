b6abd21775f0887a23ff9c626fe512b12cb619dc
mparczyk <marekparczyk@gmail.com>
2023-08-18 10:52:35 +0200

    moving index.js to main folder

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './src/reportWebVitals';
 import 'tachyons';
-import App from './containers/App.js';
+import App from './src/containers/App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
